20ef0b4b9a4a866505dd646723a0899dae2fae33
dariokarl16 <dariokarl.sl@gmail.com>
2020-05-02 19:17:11 +0200

    initial commit

---
+++ basic_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    # to je zato što default User model koje importamo ima već svoja polja - ovo služi da ta polja extendamo s nekim svojim specifičnim
+    # one to one je field relationship
+
+    portfolio_site = models.URLField(blank=True)
+
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
